114404731a2952f7612812c22c0b5b012961d98e
Grinaldi Wisnu <grinaldi.wisnu@mekari.com>
2022-03-08 16:55:45 +0700

    feat(main): add SplashScreen and MainScreen UI with recycler view and Transaction model

---
+++ app/src/main/java/id/grinaldi/zwallet/data/Transaction.kt
@@ -0,0 +1,10 @@
+package id.grinaldi.zwallet.data
+
+import android.graphics.drawable.Drawable
+
+data class Transaction(
+    val transactionImage: Drawable,
+    val transactionName: String,
+    val transactionType: String,
+    val transactionNominal: Double,
+)
---
+++ app/src/main/java/id/grinaldi/zwallet/ui/MainActivity.kt
@@ -0,0 +1,36 @@
+package id.grinaldi.zwallet.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import id.grinaldi.zwallet.R
+import id.grinaldi.zwallet.adapter.TransactionAdapter
+import id.grinaldi.zwallet.data.Transaction
+
+class MainActivity : AppCompatActivity() {
+    private val transactionData = mutableListOf<Transaction>()
+    private lateinit var transactionAdapter: TransactionAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        this.transactionAdapter = TransactionAdapter(transactionData)
+        val recycler: RecyclerView = findViewById(R.id.recyclerTransaction)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recycler.layoutManager = layoutManager
+        recycler.adapter = transactionAdapter
+        prepareData()
+    }
+
+    private fun prepareData() {
+        this.transactionData.add(Transaction(
+            transactionImage = getDrawable(R.drawable.avatar)!!,
+            transactionName = "Grinaldi Wisnu",
+            transactionNominal = 125000.00,
+            transactionType = "Transfer"
+        ))
+
+        this.transactionAdapter.notifyDataSetChanged()
+    }
+}
